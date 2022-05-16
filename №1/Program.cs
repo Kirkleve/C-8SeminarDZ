65cc47c85e6b0bec3e529dce676221516cc2ea08
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-05-17 01:24:16 +0200

    adad

---
+++ №1/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим
+следующий массив:
+9 4 2
+2 2 6
+3 4 7
+*/
+int lines = InputNumber("Введите кол-то строк");
+int colomns = InputNumber("Введите кол-во столбцов");
+int[,] array = new int[lines, colomns];
+
+FillArray(array, 1, 10);
+PrintArray(array);
+
+
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    
+    }
+}
+
+
+
+
+int InputNumber(string output)
+{
+    Console.Write(output + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[,] matr, int festNumber, int secondNumber)
+{
+    for (int lines = 0; lines < matr.GetLength(0); lines++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[lines, columns] = new Random().Next(festNumber, secondNumber);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($" {matrix[rows, columns]}");
+        }
+        Console.WriteLine();
+    }
+}
+
